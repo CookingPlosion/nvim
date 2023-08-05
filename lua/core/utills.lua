0f3744fe1e419eb3ea631048ec75144b7ac8eada
zhipeng33 <17762727385czp@gmail.com>
2023-08-05 20:30:06 +0800

    用sudo编辑文件

--- lua/core/utills.lua
+++ lua/core/utills.lua
@@ -1,7 +1,32 @@
--- 自定义函数
+---------- -- 自定义函数 -- -----------
+
+-- cmdline echo
+function Me_Echo_multiline(msg)
+    for _, s in ipairs(vim.fn.split(msg, "\n")) do
+        vim.cmd("echom '" .. s:gsub("'", "''") .. "'")
+    end
+end
+
+function Me_Info(msg)
+    vim.cmd("echohl Directory")
+    Me_Echo_multiline(msg)
+    vim.cmd("echohl None")
+end
+
+function Me_Warn(msg)
+    vim.cmd("echohl WarningMsg")
+    Me_Echo_multiline(msg)
+    vim.cmd("echohl None")
+end
+
+function Me_Err(msg)
+    vim.cmd("echohl ErrorMsg")
+    Me_Echo_multiline(msg)
+    vim.cmd("echohl None")
+end
 
 -- 判断是否为大文件
-function IsNotLargeFile()
+Me_IsNotLargeFile = function ()
 	local max_filesize = 100 * 1024 -- 100 KB
 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(0))
 	if ok and stats and stats.size > max_filesize then
@@ -11,4 +36,45 @@ function IsNotLargeFile()
 		vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 		return true
 	end
+end
+
+-- sudo 提权
+Me_Sudo_exec = function(cmd, print_output)
+    vim.fn.inputsave()
+    local password = vim.fn.inputsecret("Password: ")
+    vim.fn.inputrestore()
+    if not password or #password == 0 then
+        Me_Warn("Invalid password, sudo aborted")
+        return false
+    end
+    local out = vim.fn.system(string.format("sudo -p '' -S %s", cmd), password)
+    if vim.v.shell_error ~= 0 then
+        print("\r\n")
+        Me_Err(out)
+        return false
+    end
+    if print_output then print("\r\n", out) end
+        return true
+end
+
+-- sudo 写入
+Me_Sudo_write = function(tmpfile, filepath)
+    if not tmpfile then tmpfile = vim.fn.tempname() end
+    if not filepath then filepath = vim.fn.expand("%") end
+    if not filepath or #filepath == 0 then
+        Me_Err("E32: No file name")
+        return
+    end
+    -- `bs=1048576` is equivalent to `bs=1M` for GNU dd or `bs=1m` for BSD dd
+    -- Both `bs=1M` and `bs=1m` are non-POSIX
+    local cmd = string.format("dd if=%s of=%s bs=1048576",
+        vim.fn.shellescape(tmpfile),
+        vim.fn.shellescape(filepath))
+    -- no need to check error as this fails the entire function
+    vim.api.nvim_exec(string.format("write! %s", tmpfile), true)
+    if Me_Sudo_exec(cmd) then
+        Me_Info(string.format([[\r\n"%s" written]], filepath))
+        vim.cmd("e!")
+    end
+    vim.fn.delete(tmpfile)
 end
