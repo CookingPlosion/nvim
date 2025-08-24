8b5d3f0a580083ee0347f739e3df0db5503b2813
zhipeng33 <204039185@qq.com>
2025-08-24 15:26:49 +0800

    refactor: 修改 加载顺序

--- init.lua
+++ init.lua
@@ -1,14 +1,5 @@
 require('config.options')
 require('config.autocmds')
 require('config.keymaps')
+require('config.lsp')
 require('config.lazy')
--- require('config.lsp')
--- Load user defined settings after Lazy initialization
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'LazyVimStarted',
-  callback = function()
-    vim.schedule(function()
-      require('config.lsp')
-    end)
-  end,
-})
